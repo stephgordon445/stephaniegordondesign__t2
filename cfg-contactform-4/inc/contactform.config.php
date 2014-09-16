e5085d7bacfd11d68ff3cabe49eae99d7df52761
stephgordon445 <stephgordon445@gmail.com>
2014-09-16 13:39:00 -0500

    stephanie gordon design contact form header
    need help with the contact form in header. the parallax elements keep
    scrolling over it….

---
+++ cfg-contactform-4/inc/contactform.config.php
@@ -0,0 +1,19 @@
+<?php
+$cfg['email_address'] = 'stephgordon445@gmail.com';
+$cfg['email_from'] = '';
+$cfg['email_address_cc'] = '';
+$cfg['email_address_bcc'] = '';
+$cfg['timezone'] = 'America/Chicago';
+$cfg['form_name'] = 'My Contact Form';
+$cfg['form_validationmessage'] = 'Thank you, your message has been sent to us.<br />We will get back to you as soon as possible.';
+$cfg['form_errormessage_captcha'] = '';
+$cfg['form_errormessage_emptyfield'] = '';
+$cfg['form_errormessage_invalidemailaddress'] = 'Invalid email address';
+$cfg['form_redirecturl'] = '';
+$cfg['adminnotification_subject'] = 'New message sent from your website';
+$cfg['usernotification_inputid'] = '';
+$cfg['usernotification_insertformdata'] = '';
+$cfg['usernotification_format'] = '';
+$cfg['usernotification_subject'] = '';
+$cfg['usernotification_message'] = '';
+?>
